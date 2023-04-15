d07e2314fa98d68a89e877da99c9b12596e1901c
Abhiram <abhiram.reddy@shopup.org>
2023-04-15 10:13:43 +0530

    create home page

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ChakraProvider>
-     <RouterProvider router={router}/>
-     </ChakraProvider>
-  </React.StrictMode>
+      <RouterProvider router={router} />
+    </ChakraProvider>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
